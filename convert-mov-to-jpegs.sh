b504e2d0c6a1dad3e5405e122b949646892895c9
X782042 <kenneth.e.schlagel@kp.org>
2017-12-18 20:02:12 -0700

    pretty up output

--- convert-mov-to-jpegs.sh
+++ convert-mov-to-jpegs.sh
@@ -2,25 +2,32 @@
 
 set -e
 
+# some pretty colors
+YEWOW='\033[1;33m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 if [ "$(uname)" != "Darwin" ]; then
-    echo "I am only to be run on macOS!"
+    echo "${RED}I am only to be run on macOS!"
     exit 1;
 fi
 
 for file in *; do
     if [ ${file: -4} == ".MOV" ]; then
-        echo "${file} found, will be converted into some training jpegs"
+        echo "${YEWOW}${file} found, lets make some jpegs${NC}"
         dirname="${file%%.*}"
-        echo "creating training images in $dirname for $file"
+        echo "creating training images in /$dirname for $file"
         mkdir -p turi_files/images-raw/$dirname
         mkdir -p training_images/$dirname
         # extract frames from each .mov
         ./extract-frames.swift -inputMoviePath $file -outputImagesPath turi_files/images-raw/$dirname/
-        echo "Wih sips, downresing images"
+        echo "with sips, downresing images"
         sips --resampleHeightWidthMax 640 turi_files/images-raw/$dirname/* --out training_images/$dirname/ 1>/dev/null  2>/dev/null
-        echo "Number of $dirname images:"
+        echo "number of $dirname images:"
         ls -1 training_images/$dirname/ | wc -l
-        echo "Cleaning up now k thx bye"
-        rm -rf turi_files
     fi
 done
+
+echo "${GREEN}cleaning up now, k thx bye${NC}"
+rm -rf turi_files
